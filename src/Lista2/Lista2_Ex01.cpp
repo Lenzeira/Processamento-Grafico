ddc4f709f18706564c18e066a250ba7da17bcc88
Guilherme Lenzi <143644875+Lenzeira@users.noreply.github.com>
2025-10-08 12:04:44 -0300

    Lista2_Ex01.cpp

--- src/Lista2/Lista2_Ex01.cpp
+++ src/Lista2/Lista2_Ex01.cpp
@@ -1,46 +1,109 @@
 #include <iostream>
 #include <string>
-#include <fstream>
-#include <sstream>
+#include <assert.h>
+
+using namespace std;
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-using namespace std;
 using namespace glm;
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
-GLuint loadShader(const string& vertexPath, const string& fragmentPath); // Função modificada
+int setupShader();
 int setupGeometry();
 
 const GLuint WIDTH = 800, HEIGHT = 600;
 
+const GLchar* vertexShaderSource = R"(
+    #version 400
+    layout (location = 0) in vec3 position;
+    layout (location = 1) in vec3 color;
+    out vec3 vColor;
+    uniform mat4 projection;
+    void main()
+    {
+	    gl_Position = projection * vec4(position.x, position.y, position.z, 1.0);
+	    vColor = color;
+    }
+)";
+
+const GLchar* fragmentShaderSource = R"(
+    #version 400
+    in vec3 vColor;
+    out vec4 color;
+    void main()
+    {
+	    color = vec4(vColor,1.0);
+    }
+)";
+
 int main()
 {
 	glfwInit();
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Exercicio 1 (Shaders Externos)", nullptr, nullptr);
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "L2_Ex01 - Ortho(-10,10,-10,10)", nullptr, nullptr);
+	if (!window)
+	{
+		std::cerr << "Falha ao criar a janela GLFW" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
 	glfwMakeContextCurrent(window);
 	glfwSetKeyCallback(window, key_callback);
-	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-	// Agora a função loadShader recebe os caminhos dos arquivos
-	GLuint shaderID = loadShader("shader.vert", "shader.frag");
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cerr << "Falha ao inicializar GLAD" << std::endl;
+		return -1;
+	}
+
+	const GLubyte* renderer = glGetString(GL_RENDERER);
+	const GLubyte* version = glGetString(GL_VERSION);
+	cout << "Renderer: " << renderer << endl;
+	cout << "OpenGL version supported " << version << endl;
+
+	GLuint shaderID = setupShader();
 	GLuint VAO = setupGeometry();
+
 	glUseProgram(shaderID);
 
-	// Criação da matriz de projeção
-	mat4 projection = ortho(-10.0f, 10.0f, -10.0f, 10.0f, -1.0f, 1.0f);
+	double prev_s = glfwGetTime();
+	double title_countdown_s = 0.1;
+
+	mat4 projection = ortho(-10.0, 10.0, -10.0, 10.0, -1.0, 1.0);
 	glUniformMatrix4fv(glGetUniformLocation(shaderID, "projection"), 1, GL_FALSE, value_ptr(projection));
 
 	while (!glfwWindowShouldClose(window))
 	{
+		{
+			double curr_s = glfwGetTime();
+			double elapsed_s = curr_s - prev_s;
+			prev_s = curr_s;
+
+			title_countdown_s -= elapsed_s;
+			if (title_countdown_s <= 0.0 && elapsed_s > 0.0)
+			{
+				double fps = 1.0 / elapsed_s;
+				char tmp[256];
+				sprintf(tmp, "L2_Ex01 - Ortho(-10,10,-10,10)\tFPS %.2lf", fps);
+				glfwSetWindowTitle(window, tmp);
+				title_countdown_s = 0.1;
+			}
+		}
+
 		glfwPollEvents();
-		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glViewport(0, 0, WIDTH, HEIGHT);
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindVertexArray(0);
@@ -59,69 +122,76 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
+int setupShader()
+{
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return shaderProgram;
+}
+
 int setupGeometry()
 {
 	GLfloat vertices[] = {
-		// Posição (x,y,z)   Cor (r,g,b)
-		 0.0f,  5.0f, 0.0f,   1.0f, 0.0f, 0.0f,
-		-5.0f, -5.0f, 0.0f,   0.0f, 1.0f, 0.0f,
-		 5.0f, -5.0f, 0.0f,   0.0f, 0.0f, 1.0f,
+		// x      y      z      R     G     B
+		 0.0,   5.0,   0.0,   0.0,  1.0,  0.8,
+		-5.0,  -5.0,   0.0,   0.0,  0.6,  1.0,
+		 5.0,  -5.0,   0.0,   0.0,  0.8,  1.0,
 	};
+
 	GLuint VBO, VAO;
-	glGenVertexArrays(1, &VAO);
+
 	glGenBuffers(1, &VBO);
-	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
+
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
+
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
-	return VAO;
-}
 
-// Nova função para carregar, compilar e linkar shaders de arquivos externos
-GLuint loadShader(const string& vertexPath, const string& fragmentPath)
-{
-    string vertexCode, fragmentCode;
-    ifstream vShaderFile, fShaderFile;
-    vShaderFile.exceptions(ifstream::failbit | ifstream::badbit);
-    fShaderFile.exceptions(ifstream::failbit | ifstream::badbit);
-    try {
-        vShaderFile.open(vertexPath);
-        fShaderFile.open(fragmentPath);
-        stringstream vShaderStream, fShaderStream;
-        vShaderStream << vShaderFile.rdbuf();
-        fShaderStream << fShaderFile.rdbuf();
-        vShaderFile.close();
-        fShaderFile.close();
-        vertexCode = vShaderStream.str();
-        fragmentCode = fShaderStream.str();
-    }
-    catch (ifstream::failure& e) {
-        cerr << "ERRO::SHADER::ARQUIVO_NAO_LIDO_COM_SUCESSO" << endl;
-    }
-    const char* vShaderCode = vertexCode.c_str();
-    const char* fShaderCode = fragmentCode.c_str();
-    
-    GLuint vertex, fragment;
-    
-	vertex = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex, 1, &vShaderCode, NULL);
-    glCompileShader(vertex);
-    
-	fragment = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment, 1, &fShaderCode, NULL);
-    glCompileShader(fragment);
-    
-	GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertex);
-    glAttachShader(shaderProgram, fragment);
-    glLinkProgram(shaderProgram);
-    
-	glDeleteShader(vertex);
-    glDeleteShader(fragment);
-    return shaderProgram;
+	return VAO;
 }
